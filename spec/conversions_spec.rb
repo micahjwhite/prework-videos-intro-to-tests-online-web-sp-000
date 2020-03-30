498094968953d4dbd187fee077ba9730d13730c0
Micah White <micahjwhite@gmail.com>
2020-03-30 01:48:20 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -3,7 +3,7 @@ require_relative '../conversions.rb'
 describe "conversions" do
   describe '#ounces_to_grams' do
     it 'given zero, returns 0.0' do
-      e
+      grams = ounces_to_grams(0)
     end
   end
 end
